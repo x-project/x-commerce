736d77ecdf6a584ce82f9585dc8db5fe89ffb283
Federico Spini <federico.spini@gmail.com>
2015-10-08 19:16:30 +0200

    refactores image.js

--- server/faker.js
+++ server/faker.js
@@ -95,19 +95,19 @@ fakers['customers'] = function () {
   };
 };
 
-function random_model_from (models) {
+var random_model_from = function (models) {
   var index = faker.random.number({ min: 0, max: models.length - 1 });
   var model = models[index];
   return model;
-}
+};
 
-function random_id_from (models) {
+var random_id_from = function (models) {
   var model = random_model_from(models);
   var id = model.id;
   return id;
-}
+};
 
-function till (n, iterate) {
+var till = function (n, iterate) {
   return function (next) {
     var count = 0;
 
@@ -122,9 +122,9 @@ function till (n, iterate) {
 
     async.whilst(test, step, next);
   }
-}
+};
 
-function populate (name) {
+var populate = function (name) {
   return function (next) {
     var data = fakers[name]();
     var api = 'http://localhost:3000/api/' + name;
@@ -144,85 +144,66 @@ function populate (name) {
         next(null);
       });
   };
-}
+};
 
-function each (models, iterate) {
+var each = function (models, iterate) {
   return function (next) {
     async.eachSeries(models, iterate, next);
   };
 };
 
-function populate_image (name) {
-  return function (item, next) {
-
-    var image = random_model_from(images);
-
-    function signed_url (next) {
-      request
-        .post('http://localhost:3000' + '/api/' + name + '/' + item.id + '/images')
-        .send({
-          filename: image,
-          filetype: "image/jpeg",
-          container: name + '/' + item.id + '/images'
-        })
-        .type('json')
-        .set('Accept', 'application/json')
-        .end(function (err, res) {
-          if (err) {
-            next(err);
-            return;
-          }
-          next(null, res.body);
-        });
-    }
-
-    function upload_image (data, next) {
-      request
-        .post('http://localhost:3000' + data.signed_url)
-        .attach('file', __dirname + '/images/' + data.filename)
-        .end(next);
-    }
-
-    var operation = retry.operation();
-    operation.attempt(function () {
-      async.waterfall([
-        signed_url,
-        upload_image
-      ], function (err) {
-        console.log(err);
-        if (operation.retry(err)) {
-          return;
-        }
-        setImmediate(next, err ? operation.mainError() : null);
+var signed_url = function (name, item, image) {
+  return function (next) {
+    request
+      .post('http://localhost:3000' + '/api/' + name + '/' + item.id + '/images')
+      .send({
+        filename: image,
+        filetype: "image/jpeg",
+        container: name + '/' + item.id + '/images'
+      })
+      .type('json')
+      .set('Accept', 'application/json')
+      .end(function (err, res) {
+        setImmediate(next, err, res.body);
       });
-    });
   };
-}
+};
 
-function load_images (path) {
+var upload_image = function (data, next) {
+  request
+    .post('http://localhost:3000' + data.signed_url)
+    .attach('file', __dirname + '/images/' + data.filename, data.filename)
+    .end(next);
+};
+
+var populate_image = function (name) {
+  return function (item, next) {
+    var image = random_model_from(images);
+    async.waterfall([
+      signed_url(name, item, image),
+      upload_image
+    ], next);
+  };
+};
+
+var load_images = function (path) {
   return function (next) {
     fs.readdir(path, function (err, files) {
-      if (err) {
-        images = [];
-        next(err);
-        return;
-      }
-      images = files;
-      console.log(images);
-      next(null);
+      images = err ? [] : files;
+      setImmediate(next, err);
     });
   };
 }
 
 function start () {
   async.waterfall([
-    // till(3, populate('product_types')),
-    // till(3, populate('customers')),
-    // till(3, populate('vendors')),
-    till(3, populate('collections')),
-    // till(30, populate('products')),
+    till(4, populate('product_types')),
+    till(4, populate('customers')),
+    till(6, populate('vendors')),
+    till(4, populate('collections')),
+    till(20, populate('products')),
     load_images(__dirname + '/images'),
-    // each(collections['products'], populate_image('products')),
+    each(collections['products'], populate_image('products')),
     each(collections['collections'], populate_image('collections'))
   ], function (err) {
     if (err) {
@@ -230,6 +211,7 @@ function start () {
       return;
     }
     console.log('yeah');
+    process.exit(1);
   });
 }
 
