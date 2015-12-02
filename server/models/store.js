90cfcaf49fc1302fa54899cce57b4feda6beab60
BALJINDER JIT <bal.jit1590@gmail.com>
2015-12-02 21:54:45 +0100

    data validation on page-store

--- server/models/store.js
+++ server/models/store.js
@@ -1,3 +1,7 @@
 module.exports = function (Store) {
 
+  var email_pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  Store.validatesFormatOf('email', {with: email_pattern, message: 'Must provide a valid email'});
+
 };
